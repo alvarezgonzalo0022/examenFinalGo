21aad4644412e2745e980bce48dd13ace8693021
anagomezmignot <ana.gomezmignot@gmail.com>
2023-11-27 23:05:54 -0300

    handler

--- cmd/server/handler/turnos/turnos.go
+++ cmd/server/handler/turnos/turnos.go
@@ -1,9 +1,7 @@
 package turnos
 
 import (
-	"errors"
-	"log"
-	"time"
+	"context"
 	"github.com/alvarezgonzalo0022/examenFinalGo/internal/domain"
 	"github.com/alvarezgonzalo0022/examenFinalGo/internal/turnos"
 	"github.com/alvarezgonzalo0022/examenFinalGo/pkg/web"
@@ -18,5 +16,19 @@ import (
 			service: service,
 		}
 	}	
+ 
+	func (h *turnoHandler) Create(c *gin.Context) {
+		var turno domain.Turno
+		if err := c.ShouldBindJSON(&turno); err != nil {
+			web.NewResponse(c).BadRequest(err)
+			return
+		}
+		turno, err := h.service.Create(context.Background(), turno)
+		if err != nil {
+			web.NewResponse(c).InternalServerError(err)
+			return
+		}
+		web.NewResponse(c).Ok(turno)
+	}
 
 	
